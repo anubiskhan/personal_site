4b1ac1c0a9176f748e5ec471a538f86c22a4519e
Kelly Schroeder <kellyrschroeder@gmail.com>
2021-11-21 22:19:58 -0800

    Add flex, styling, globals

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Allo, World!"/>
+  <Hero/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hero
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
+    <nav class="nav">  
+        <div class="me">Me</div>  
+        <div class="creations">Creations</div>  
+    </nav>
+    <div class="body">
         
     </div>
 </template>
 <script>
 export default {
-    
+    name: 'Hero'
 }
 </script>
-<style>
-    
+<style lang="scss">
+    nav {
+        display: flex;
+        justify-content: flex-start;
+        div {
+            border: 2px solid black;
+            flex: 1;
+        }
+    }
 </style>
