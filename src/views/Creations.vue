332e7759467eea2caabfc976b6a83c1c95c19341
Kelly Schroeder <kellyrschroeder@gmail.com>
2021-11-30 19:28:27 -0800

    Add json data file and import

--- src/views/Creations.vue
+++ src/views/Creations.vue
@@ -1,11 +1,20 @@
 <template lang="">
     <div>
         <h1>This is CREATIONS</h1>
+        <ol>
+            <li v-for="creation in myCreations" :key="creation.id"> {{ creation.title }}</li>
+        </ol>
     </div>
 </template>
 <script>
+import creations from '@/json/creations.json'
 export default {
-    name: 'Creations'
+    name: 'Creations',
+    data() {
+        return {
+            myCreations: creations
+        }
+    }
 }
 </script>
 <style lang="scss">
