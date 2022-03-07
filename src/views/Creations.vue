5cbce9bc588af3777f9cf16a83f70dde3243c998
Kelly Schroeder <kellyrschroeder@gmail.com>
2022-03-06 21:51:57 -0800

    Remove filler copy from Creations

--- src/views/Creations.vue
+++ src/views/Creations.vue
@@ -1,10 +1,6 @@
 <template lang="">
     <div class="creationsContainer">
         <h1>This is CREATIONS</h1>
-        <h3>this is a list</h3>
-        <ol>
-            <li v-for="creation in myCreations" :key="creation.id"> {{ creation.title }}</li>
-        </ol>
     </div>
 </template>
 <script>
@@ -19,9 +15,5 @@ export default {
 }
 </script>
 <style lang="scss">
-    .creationsContainer {
-        color: $orange;
-        display: grid;
-        grid-template-columns: 1fr;
-    }
+
 </style>
